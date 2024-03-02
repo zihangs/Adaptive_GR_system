e98617bb2464133497ccaa6fa00a72aece03aeb9
zihangs <suzihang0612@gmail.com>
2024-03-02 10:34:24 +0800

    add real world experiments and results

---
+++ run_all_real_world.py
@@ -0,0 +1,31 @@
+import os
+
+os.system("python3 relearn_grsystem_real.py -no-relearn sepsis_cases_2 1.0")
+os.system("python3 relearn_grsystem_real.py -openloop sepsis_cases_2 1.0")
+os.system("python3 relearn_grsystem_real.py -closedloop_ave_metric sepsis_cases_2 1.0")
+os.system("python3 relearn_grsystem_real.py -closedloop-trend sepsis_cases_2 1.0")
+
+# os.system("python3 relearn_grsystem_real.py -no-relearn sepsis_cases_1 1.0")
+
+# os.system("python3 relearn_grsystem_real.py -no-relearn BPIC11_f2 1.0")
+# os.system("python3 relearn_grsystem_real.py -openloop BPIC11_f2 1.0")
+# os.system("python3 relearn_grsystem_real.py -closedloop_ave_metric BPIC11_f2 1.0")
+# os.system("python3 relearn_grsystem_real.py -closedloop-trend BPIC11_f2 1.0")
+
+
+
+
+# os.system("python3 relearn_grsystem_real.py -no-relearn hospital_billing_2 1.0")
+# os.system("python3 relearn_grsystem_real.py -openloop hospital_billing_2 1.0")
+
+# os.system("python3 relearn_grsystem_real.py -no-relearn hospital_billing_3 1.0")
+# os.system("python3 relearn_grsystem_real.py -openloop hospital_billing_3 1.0")
+
+
+# os.system("python3 relearn_grsystem_real.py -closedloop_ave_metric hospital_billing_2 1.0")
+# os.system("python3 relearn_grsystem_real.py -closedloop-trend hospital_billing_2 1.0")
+
+# os.system("python3 relearn_grsystem_real.py -closedloop_ave_metric hospital_billing_3 1.0")
+# os.system("python3 relearn_grsystem_real.py -closedloop-trend hospital_billing_3 1.0")
+
+
